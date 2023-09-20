c6799aa46cda6cad07aba193194ed929f46dbc96
Dominik Suszek <dominik.suszek@gmail.com>
2023-09-20 15:35:05 +0200

    test: new UI tests for Employee class

--- test/ui/EmployeeTests.js
+++ test/ui/EmployeeTests.js
@@ -37,6 +37,7 @@ describe('Employee test', async () => {
 
         await driver.quit();
     });
+
     /*
     UI Test Exercise 1
 
@@ -48,6 +49,37 @@ describe('Employee test', async () => {
 
     This should pass without code changes
     */
+    it('Create new employee with a salary of £10,000', async () => {
+        var driver = new webdriver.Builder().
+            withCapabilities(webdriver.Capabilities.chrome()).
+            build();
+
+        await driver.get(process.env.UI_TEST_URL);
+
+        await driver.findElement(webdriver.By.id('add-employee-button')).click();
+
+        await driver.findElement(webdriver.By.id('fname')).sendKeys('UI');
+        await driver.findElement(webdriver.By.id('lname')).sendKeys('Tests');
+        await driver.findElement(webdriver.By.id('email')).sendKeys('testemail@email.com');
+        await driver.findElement(webdriver.By.id('address')).sendKeys('1 Home Street');
+        await driver.findElement(webdriver.By.id('address2')).sendKeys('Home Lane');
+        await driver.findElement(webdriver.By.id('city')).sendKeys('Belfast');
+        await driver.findElement(webdriver.By.id('county')).sendKeys('Antrim');
+        await driver.findElement(webdriver.By.id('postalCode')).sendKeys('BT9');
+        await driver.findElement(webdriver.By.id('country')).sendKeys('Norn Iron');
+        await driver.findElement(webdriver.By.id('phoneNo')).sendKeys('01234567890');
+        await driver.findElement(webdriver.By.id('bankNo')).sendKeys('12345678');
+        await driver.findElement(webdriver.By.id('nin')).sendKeys('AA1A11AA');
+        await driver.findElement(webdriver.By.id('salary')).sendKeys('10000');
+        await driver.findElement(webdriver.By.id('submit')).click('#submit');
+   
+        await driver.findElement(webdriver.By.id('create-employee-error')).getText().then(function(value) {
+            chai.assert.equal(value, 'Salary must be at least £20,000');
+        });
+
+        await driver.quit();
+    });
+
 
     /*
     UI Test Exercise 2
@@ -60,6 +92,35 @@ describe('Employee test', async () => {
 
     This should pass without code changes
     */
+    it('Create new employee with a salary of ABC', async () => {
+        var driver = new webdriver.Builder().
+            withCapabilities(webdriver.Capabilities.chrome()).
+            build();
+
+        await driver.get(process.env.UI_TEST_URL);
+
+        await driver.findElement(webdriver.By.id('add-employee-button')).click();
+        await driver.findElement(webdriver.By.id('fname')).sendKeys('UI');
+        await driver.findElement(webdriver.By.id('lname')).sendKeys('Tests');
+        await driver.findElement(webdriver.By.id('email')).sendKeys('testemail@email.com');
+        await driver.findElement(webdriver.By.id('address')).sendKeys('1 Home Street');
+        await driver.findElement(webdriver.By.id('address2')).sendKeys('Home Lane');
+        await driver.findElement(webdriver.By.id('city')).sendKeys('Belfast');
+        await driver.findElement(webdriver.By.id('county')).sendKeys('Antrim');
+        await driver.findElement(webdriver.By.id('postalCode')).sendKeys('BT9');
+        await driver.findElement(webdriver.By.id('country')).sendKeys('Norn Iron');
+        await driver.findElement(webdriver.By.id('phoneNo')).sendKeys('01234567890');
+        await driver.findElement(webdriver.By.id('bankNo')).sendKeys('12345678');
+        await driver.findElement(webdriver.By.id('nin')).sendKeys('AA1A11AA');
+        await driver.findElement(webdriver.By.id('salary')).sendKeys('ABC');
+        await driver.findElement(webdriver.By.id('submit')).click('#submit');
+   
+        await driver.findElement(webdriver.By.id('create-employee-error')).getText().then(function(value) {
+            chai.assert.equal(value, 'Salary must be a number');
+        });
+
+        await driver.quit();
+    });
 
     /*
     UI Test Exercise 3
@@ -72,6 +133,25 @@ describe('Employee test', async () => {
 
     Expect the name on the view employee page to match the name from the link you've clicked
     */
+    it('Select view on an employee', async () => {
+        var driver = new webdriver.Builder().
+            withCapabilities(webdriver.Capabilities.chrome()).
+            build();
+
+        await driver.get(process.env.UI_TEST_URL);
+        await driver.findElement(webdriver.By.id('view-employees-button')).click();
+        var Forename = await driver.findElement(webdriver.By.xpath('/html/body/div/main/table/tbody/tr[1]/td[1]')).getText();
+
+        await driver.findElement(webdriver.By.id('view_employee_0')).click();
+        
+        await driver.findElement(webdriver.By.xpath('/html/body/div/main/table/tbody/tr/td[1]')).getText().then(function(value) {
+            chai.assert.equal(value, Forename);
+        });
+        await driver.quit();
+    });
+
+
+
 
     /*
     UI Test Exercise 4
@@ -82,5 +162,24 @@ describe('Employee test', async () => {
 
     Expect 'Employee does not exist' error to be displayed
     */
+    it('Select view on an employee', async () => {
 
+        it.only('Select view on an employee', async () => {
+            
+            const driver = new webdriver.Builder()
+                .withCapabilities(webdriver.Capabilities.chrome())
+                .build();
+        
+            try {
+                await driver.get(process.env.UI_TEST_URL + '/employees/52343');
+        
+                const errorMessage = await driver.findElement(webdriver.By.id('error-message')).getText();
+                chai.assert.equal(errorMessage, 'Employee does not exist');
+            } catch (error) {
+                console.error('An error occurred:', error);
+            } finally {
+                await driver.quit();
+            }
+        });
+    });
   })
